46b88c495f84e9c3c43c2565b9b6863043c8273e
Alexey Shabginov <a.shabginov@gmail.com>
2020-05-22 20:09:25 -0700

    Passes likes through props

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -4,6 +4,7 @@
 import React from "react";
 
 const LikeSection = props => {
+  console.log(props.likes);
   return (
     <div>
     <div
@@ -17,9 +18,7 @@ const LikeSection = props => {
         <i className="far fa-comment" />
       </div>
     </div>
-    <p className="like-number">
-      
-      likes</p>
+  <p className="like-number">{props.likes} likes</p>
 </div>
   )
 };
